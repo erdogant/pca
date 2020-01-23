3616c1a45efc254e6e3abcf745d0f8d8b71262c5
erdogant <erdogant@gmail.com>
2020-01-23 22:42:23 +0100

    out changed into model. input parameters changed. widht and heights into figsize()

--- pca/pca.py
+++ pca/pca.py
@@ -185,47 +185,63 @@ def biplot(model, figsize=(10,8)):
         newx=newx*figscaling*0.5
         newy=newy*figscaling*0.5
         ax.arrow(0, 0, newx, newy, color='r', width=0.005, head_width=0.05, alpha=0.6)
-        ax.text(newx*1.25, newy*1.25, out['toplabels'][i], color='black', ha='center', va='center')
+        ax.text(newx*1.25, newy*1.25, model['col_labels'][i], color='black', ha='center', va='center')
 
     plt.show()
-    return(ax)
+    return(fig, ax)
 
-#%% biplot3d
-def biplot3d(out, height=8, width=10, xlim=[], ylim=[]):
-    assert out['X'].shape[1]>2, print('[PCA] Requires 3 PCs to make 3d plot. Try pca.biplot()')
+
+# %% biplot3d
+def biplot3d(model, figsize=(10,8)):
+    """
+    
+
+    Parameters
+    ----------
+    model : dict
+        model created by the fit() function.
+    figsize : (float, float), optional, default: None
+        (width, height) in inches. If not provided, defaults to rcParams["figure.figsize"] = (10,8)
+
+    Returns
+    -------
+    tuple containing (fig, ax)
+
+    """
+    assert model['X'].shape[1]>2, print('[PCA] Requires 3 PCs to make 3d plot. Try pca.biplot()')
 
     # Get coordinates
-    xs = out['X'][:,0]
-    ys = out['X'][:,1]
-    zs = out['X'][:,2]
+    xs = model['X'][:,0]
+    ys = model['X'][:,1]
+    zs = model['X'][:,2]
 
     # Figure
-    fig = plt.figure(1, figsize=(width, height))
+    fig = plt.figure(1, figsize=figsize)
     ax = Axes3D(fig, rect=[0, 0, .95, 1], elev=48, azim=134)
     # Make scatter plot
-    uiy=np.unique(out['y'])
-    getcolors=_discrete_cmap(len(uiy))
+    uiy=np.unique(model['y'])
+    getcolors=colourmap.generate(len(uiy))
     for i,y in enumerate(uiy):
-        I=y==out['y']
+        I=y==model['y']
         getcolors[i,:]
         ax.scatter(xs[I],ys[I],zs[I],color=getcolors[i,:], s=25)
 
     # Set y
-    ax.set_xlabel('PC1 ('+ str(out['model'].explained_variance_ratio_[0]*100)[0:4] + '% expl.var)')
-    ax.set_ylabel('PC2 ('+ str(out['model'].explained_variance_ratio_[1]*100)[0:4] + '% expl.var)')
-    ax.set_zlabel('PC3 ('+ str(out['model'].explained_variance_ratio_[2]*100)[0:4] + '% expl.var)')
-    ax.set_title('Components that cover the [' + str(out['pcp']) + '] explained variance, PC=['+ str(out['topn'])+  ']')
+    ax.set_xlabel('PC1 ('+ str(model['model'].explained_variance_ratio_[0]*100)[0:4] + '% expl.var)')
+    ax.set_ylabel('PC2 ('+ str(model['model'].explained_variance_ratio_[1]*100)[0:4] + '% expl.var)')
+    ax.set_zlabel('PC3 ('+ str(model['model'].explained_variance_ratio_[2]*100)[0:4] + '% expl.var)')
+    ax.set_title('Components that cover the [' + str(model['pcp']) + '] explained variance, PC=['+ str(model['topn'])+  ']')
 
     #% Gather top N loadings
-    I = top_scoring_components(out['loadings'], out['topn'])
-    xvector = out['loadings'][0,I]
-    yvector = out['loadings'][1,I]
-    zvector = out['loadings'][2,I]
+    I = top_scoring_components(model['loadings'], model['topn'])
+    xvector = model['loadings'][0,I]
+    yvector = model['loadings'][1,I]
+    zvector = model['loadings'][2,I]
     
     # Plot and scale values for arrows and text
-    scalex = 1.0/(out['loadings'][0,:].max() - out['loadings'][0,:].min())
-    scaley = 1.0/(out['loadings'][1,:].max() - out['loadings'][1,:].min())
-    scalez = 1.0/(out['loadings'][2,:].max() - out['loadings'][2,:].min())
+    scalex = 1.0/(model['loadings'][0,:].max() - model['loadings'][0,:].min())
+    scaley = 1.0/(model['loadings'][1,:].max() - model['loadings'][1,:].min())
+    scalez = 1.0/(model['loadings'][2,:].max() - model['loadings'][2,:].min())
     # Plot the arrows
     for i in range(len(xvector)):
         # arrows project features (ie columns from csv) as vectors onto PC axes
@@ -238,32 +254,34 @@ def biplot3d(out, height=8, width=10, xlim=[], ylim=[]):
         newy=newy*figscaling*0.5
         newz=newz*figscaling*0.5
         # ax.arrow(0, 0, newx/20, newy/20, color='r', width=0.0005, head_width=0.005, alpha=0.6)
-        ax.text(newx, newy, newz, out['toplabels'][i], color='black', ha='center', va='center')
+        ax.text(newx, newy, newz, model['col_labels'][i], color='black', ha='center', va='center')
 
     plt.show()
     return(ax)
 
-#%% Show explained variance plot
-def plot(out, height=8, width=10):
-    [fig,ax]=plt.subplots(figsize=(width,height), edgecolor='k')
-    plt.plot(np.append(0,out['explained_var']),'o-', color='k', linewidth=1)
+
+# %% Show explained variance plot
+def plot(model, figsize=(10,8)):
+    [fig,ax]=plt.subplots(figsize=figsize, edgecolor='k')
+    plt.plot(np.append(0,model['explained_var']),'o-', color='k', linewidth=1)
     plt.ylabel('Percentage explained variance')
     plt.xlabel('Principle Component')
-    plt.xticks(np.arange(0,len(out['explained_var'])+1))
+    plt.xticks(np.arange(0,len(model['explained_var'])+1))
     plt.ylim([0,1])
-    titletxt='Cumulative explained variance\nMinimum components that cover the [' + str(out['pcp']) + '] explained variance, PC=['+ str(out['topn'])+  ']'
+    titletxt='Cumulative explained variance\nMinimum components that cover the [' + str(model['pcp']) + '] explained variance, PC=['+ str(model['topn'])+  ']'
     plt.title(titletxt)
     plt.grid(True)
 
     # Plot vertical line To stress the cut-off point
     # ax.axvline(x=eps[idx], ymin=0, ymax=sillclust[idx], linewidth=2, color='r')
-    ax.axhline(y=out['pcp'], xmin=0, xmax=1, linewidth=0.8, color='r')
-    plt.bar(np.arange(0,len(out['explained_var'])+1),np.append(0,out['model'].explained_variance_ratio_),color='#3182bd', alpha=0.8)
+    ax.axhline(y=model['pcp'], xmin=0, xmax=1, linewidth=0.8, color='r')
+    plt.bar(np.arange(0,len(model['explained_var'])+1),np.append(0,model['model'].explained_variance_ratio_),color='#3182bd', alpha=0.8)
     plt.show()
     plt.draw()
     return(ax)
+
     
-#%% Top scoring components
+# %% Top scoring components
 def top_scoring_components(loadings, topn):
     # Top scoring for 1st component
     I1=np.argsort(np.abs(loadings[0,:]))
@@ -280,7 +298,8 @@ def top_scoring_components(loadings, topn):
     I = [I[index] for index in sorted(indices)]
     return(I)
 
-#%% Top scoring components
+
+# %% Top scoring components
 def norm(X, pcp=1, pcexclude=[1], savemem=False):
     '''
     Normalize your data using the principal components.
