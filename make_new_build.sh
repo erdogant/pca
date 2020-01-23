bf68482a3d9d79a1d5cf35f8ffce4d904605cf0b
erdogant <erdogant@gmail.com>
2020-01-23 22:40:39 +0100

    nieuw version

--- make_new_build.sh
+++ make_new_build.sh
@@ -3,17 +3,14 @@ rm -rf dist
 rm -rf build
 rm -rf pca.egg-info
 
-read -p "Making source build after pressing [Enter].."
 echo "Making new build.."
 echo ""
 python setup.py bdist_wheel
 echo ""
-read -p "Making source build after pressing [Enter].."
 echo 
 python setup.py sdist
 echo ""
-read -p "Press [Enter] to install the pip package..."
-pip install -U dist/pca-0.1.2-py3-none-any.whl
+pip install -U dist/pca-0.1.3-py3-none-any.whl
 echo ""
 read -p "[PYPI] [python -m twine upload dist/*]"
 echo ""
--- pca/__init__.py
+++ pca/__init__.py
@@ -8,6 +8,7 @@ from pca.pca import (
 
 __author__ = 'Erdogan Tasksen'
 __email__ = 'erdogant@gmail.com'
+__version__ = '0.1.3'
 
 # module level doc-string
 __doc__ = """
