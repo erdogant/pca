12d2291d5e2ab424985cb8108afad7376fb745d1
erdogant <erdogant@gmail.com>
2020-03-08 22:52:12 +0100

    make build file

--- make_new_build.sh
+++ make_new_build.sh
@@ -3,15 +3,21 @@ rm -rf dist
 rm -rf build
 rm -rf pca.egg-info
 
-echo "Making new build.."
+echo "Making new wheel.."
 echo ""
 python setup.py bdist_wheel
 echo ""
-echo 
+
+echo "Making source build .."
+echo ""
 python setup.py sdist
 echo ""
+
+read -p "Press [Enter] to install the pip package..."
 pip install -U dist/pca-0.1.3-py3-none-any.whl
 echo ""
-read -p "[PYPI] [python -m twine upload dist/*]"
+
+read -p ">twine upload dist/* TO UPLOAD TO PYPI..."
 echo ""
+
 read -p "Press [Enter] key to close window..."
