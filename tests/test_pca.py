b65fe15242e56bc1f76f0d17ffa99e12c87be870
erdogant <erdogant@gmail.com>
2020-03-21 12:00:42 +0100

    unit test plt removed

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -3,7 +3,7 @@ import numpy as np
 from sklearn.datasets import load_iris
 from sklearn.model_selection import GridSearchCV
 import itertools as it
-
+import matplotlib.pyplot as plt
 
 def test_pca():
     ######## TEST 1 #########
@@ -24,22 +24,22 @@ def test_pca():
     for combination in combinations:
         model = pca.fit(X, n_components=combination[0], row_labels=combination[1], col_labels=combination[2])
         ax = pca.plot(model)
-        plt.close('all')
+        # plt.close('all')
         ax = pca.biplot(model)
-        plt.close('all')
+        # plt.close('all')
         ax = pca.biplot3d(model)
-        plt.close('all')
+        # plt.close('all')
 
     ######## TEST 2 #########
     X = sparse_random(100, 1000, density=0.01, format='csr',random_state=42)
     
     model = pca.fit(X)
     ax = pca.plot(model)
-    plt.close('all')
+    # plt.close('all')
     ax = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     ax = pca.biplot3d(model)
-    plt.close('all')
+    # plt.close('all')
     
     ######## TEST 3 #########
     X = load_iris().data
@@ -48,33 +48,33 @@ def test_pca():
     
     model = pca.fit(X)
     ax = pca.plot(model)
-    plt.close('all')
+    # plt.close('all')
     ax = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     ax = pca.biplot3d(model)
-    plt.close('all')
+    # plt.close('all')
     
     model = pca.fit(X, row_labels=y, col_labels=labels)
     fig = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     fig = pca.biplot3d(model)
-    plt.close('all')
+    # plt.close('all')
     
     model = pca.fit(X, n_components=0.95)
     ax = pca.plot(model)
-    plt.close('all')
+    # plt.close('all')
     ax   = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     
     model = pca.fit(X, n_components=2)
     ax = pca.plot(model)
-    plt.close('all')
+    # plt.close('all')
     ax   = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     
     Xnorm = pca.norm(X, pcexclude=[1,2])
     model = pca.fit(Xnorm, row_labels=y, col_labels=labels)
     ax = pca.biplot(model)
-    plt.close('all')
+    # plt.close('all')
     ax = pca.plot(model)
-    plt.close('all')
+    # plt.close('all')
