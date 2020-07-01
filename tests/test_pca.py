1e6a12202f5c480965f1b597220436f4cfff1a49
Erdogan Taskesen <erdogant@gmail.com>
2020-07-01 22:35:27 +0200

    new unit tests

--- tests/test_pca.py
+++ tests/test_pca.py
@@ -1,5 +1,6 @@
-import pca
+from pca import pca
 import numpy as np
+import pandas as pd
 from sklearn.datasets import load_iris
 from sklearn.model_selection import GridSearchCV
 import itertools as it
@@ -10,6 +11,8 @@ def test_pca():
     X = load_iris().data
     labels=load_iris().feature_names
     y=load_iris().target
+    
+    X = pd.DataFrame(data=load_iris().data, columns=load_iris().feature_names, index=load_iris().target)
 
     param_grid = {
         'n_components':[None, 0.01, 1, 0.95, 2, 100000000000],
@@ -22,59 +25,65 @@ def test_pca():
     combinations=list(combinations)
 
     for combination in combinations:
-        model = pca.fit(X, n_components=combination[0], row_labels=combination[1], col_labels=combination[2])
-        ax = pca.plot(model)
-        # plt.close('all')
-        ax = pca.biplot(model)
-        # plt.close('all')
-        ax = pca.biplot3d(model)
-        # plt.close('all')
+        model = pca(n_components=combination[0])
+        model.fit_transform(X)
+        assert model.plot()
+        assert model.biplot()
+        assert model.biplot3d()
+
+
+
+    ### TEST CORRRECT ORDERING FEATURES IN BIPLOT
+    f1=np.random.randint(0,100,250)
+    f2=np.random.randint(0,50,250)
+    f3=np.random.randint(0,25,250)
+    f4=np.random.randint(0,10,250)
+    f5=np.random.randint(0,5,250)
+    f6=np.random.randint(0,4,250)
+    f7=np.random.randint(0,3,250)
+    f8=np.random.randint(0,2,250)
+    f9=np.random.randint(0,1,250)
+    X = np.c_[f1,f2,f3,f4,f5,f6,f7,f8,f9]
+    X = pd.DataFrame(data=X, columns=['f1','f2','f3','f4','f5','f6','f7','f8','f9'])
+    
+    model = pca(n_components=9, normalize=False)
+    out = model.fit_transform(X)
+    assert np.all(out['topfeat'].feature.values == ['f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9'])
+    assert model.biplot(n_feat=10, legend=False)
+    assert model.biplot3d(n_feat=10, legend=False)
+
+    ##### NORMALIZE OUT PC1
+    X_norm = model.norm(X, pcexclude=[1])
+    X_norm = pd.DataFrame(data=X_norm, columns=['f1','f2','f3','f4','f5','f6','f7','f8','f9'])
+    out = model.fit_transform(X_norm)
+    assert out['topfeat'].feature.values[-1]=='f1'
+    assert out['topfeat'].feature.values[0]=='f2'
+    
+    ##### NORMALIZE OUT PC1 AND PC2
+    X_norm = model.norm(X, pcexclude=[1,2])
+    X_norm = pd.DataFrame(data=X_norm, columns=['f1','f2','f3','f4','f5','f6','f7','f8','f9'])
+    out = model.fit_transform(X_norm)
+    assert out['topfeat'].feature.values[-2]=='f1'
+    assert out['topfeat'].feature.values[-1]=='f2'
+    assert out['topfeat'].feature.values[0]=='f3'
+
+    ##### NORMALIZE OUT PC2 AND PC4
+    X_norm = model.norm(X, pcexclude=[2])
+    X_norm = pd.DataFrame(data=X_norm, columns=['f1','f2','f3','f4','f5','f6','f7','f8','f9'])
+    out = model.fit_transform(X_norm)
+    assert out['topfeat'].feature.values[-1]=='f2'
+    assert out['topfeat'].feature.values[1]=='f3'
+
 
     ######## TEST 2 #########
-    X = sparse_random(100, 1000, density=0.01, format='csr',random_state=42)
+    # X = sparse_random(100, 1000, density=0.01, format='csr',random_state=42)
     
-    model = pca.fit(X)
-    ax = pca.plot(model)
-    # plt.close('all')
-    ax = pca.biplot(model)
-    # plt.close('all')
-    ax = pca.biplot3d(model)
-    # plt.close('all')
+    # model = pca.fit(X)
+    # ax = pca.plot(model)
+    # # plt.close('all')
+    # ax = pca.biplot(model)
+    # # plt.close('all')
+    # ax = pca.biplot3d(model)
+    # # plt.close('all')
     
-    ######## TEST 3 #########
-    X = load_iris().data
-    labels=load_iris().feature_names
-    y=load_iris().target
-    
-    model = pca.fit(X)
-    ax = pca.plot(model)
-    # plt.close('all')
-    ax = pca.biplot(model)
-    # plt.close('all')
-    ax = pca.biplot3d(model)
-    # plt.close('all')
-    
-    model = pca.fit(X, row_labels=y, col_labels=labels)
-    fig = pca.biplot(model)
-    # plt.close('all')
-    fig = pca.biplot3d(model)
-    # plt.close('all')
-    
-    model = pca.fit(X, n_components=0.95)
-    ax = pca.plot(model)
-    # plt.close('all')
-    ax   = pca.biplot(model)
-    # plt.close('all')
-    
-    model = pca.fit(X, n_components=2)
-    ax = pca.plot(model)
-    # plt.close('all')
-    ax   = pca.biplot(model)
-    # plt.close('all')
-    
-    Xnorm = pca.norm(X, pcexclude=[1,2])
-    model = pca.fit(Xnorm, row_labels=y, col_labels=labels)
-    ax = pca.biplot(model)
-    # plt.close('all')
-    ax = pca.plot(model)
-    # plt.close('all')
+   
