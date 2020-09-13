eba103fbe73c11d4ab94f975394d5efbc6af1c1b
Erdogan Taskesen <erdogant@gmail.com>
2020-09-13 22:12:04 +0200

    1.2.0

--- make_clean.sh
+++ make_clean.sh
@@ -5,3 +5,11 @@ rm -rf pca.egg-info
 rm -rf __pycache__
 rm -rf .pytest_cache
 rm -rf .pylint.d
+rm -rf pca/data/*.zip
+rm -rf pca/data/*.csv
+rm -rf pca/__pycache__
+rm -rf *.js
+rm -rf *.html
+rm -rf *.css
+rm -rf *.dot
+rm -rf *.png
