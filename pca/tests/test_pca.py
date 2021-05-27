515ee77139194ea40512b62c56cc2715b216e7b5
erdogant <erdogant@gmail.com>
2021-05-27 14:54:17 +0200

    unit test for outlier detection after mapping

--- pca/tests/test_pca.py
+++ pca/tests/test_pca.py
@@ -121,3 +121,26 @@ class TestPCA(unittest.TestCase):
 		assert model.scatter3d(alpha_transparency=0.5)
 		assert model.biplot(alpha_transparency=0.5)
 		assert model.biplot3d(alpha_transparency=0.5)
+
+	def test_for_new_outliers_after_transformation(self):
+        # Generate dataset
+        np.random.seed(42)
+        n_total, train_ratio = 10000, 0.8
+        n_features = 10
+        my_array = np.random.randint(low=1, high=10, size=(n_total, n_features))
+        features = [f'f{i}' for i in range(1, n_features+1, 1)]
+        X = pd.DataFrame(my_array, columns=features)
+        X_train = X.sample(frac=train_ratio)
+        X_test = X.drop(X_train.index)
+
+        # Training
+        model = pca(n_components=5, alpha=0.5, n_std=3, normalize=True, random_state=42)
+        results = model.fit_transform(X=X_train[features])
+
+        # Inference: mapping of data into space.
+        PC_test = model.transform(X=X_test[features])
+        # Compute new outliers
+        scores, _ = model.compute_outliers(PC=PC_test, n_std=3, verbose=3) 
+
+        assert model.results.get('outliers_params', None) is not None
+        assert scores.shape[0]==X_test.shape[0]
