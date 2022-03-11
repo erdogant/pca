d75873a7488dfbb4d6faf743e10804232b545e7f
erdogant <erdogant@gmail.com>
2022-03-11 14:00:22 +0100

    update url

--- setup.py
+++ setup.py
@@ -22,7 +22,7 @@ setuptools.setup(
      description="pca is a python package that performs the principal component analysis and makes insightful plots.",
      long_description=long_description,
      long_description_content_type="text/markdown",
-     url="https://github.com/erdogant/pca",
+     url="https://erdogant.github.io/pca",
 	 download_url = 'https://github.com/erdogant/pca/archive/'+new_version+'.tar.gz',
      packages=setuptools.find_packages(), # Searches throughout all dirs for files to include
      include_package_data=True, # Must be true to include files depicted in MANIFEST.in
