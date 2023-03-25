a6e66e1c431042d8d18d59d65d5ffa1f0d7c5a6e
erdogant <erdogant@gmail.com>
2023-03-25 14:43:56 +0100

    update req and setup

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ else:
 with open("README.md", "r") as fh:
     long_description = fh.read()
 setuptools.setup(
-     install_requires=['statsmodels', 'wget','matplotlib','numpy','scikit-learn','scipy','colourmap>=1.1.10','pandas','tqdm','scatterd>=1.2.5', 'adjustText'],
+     install_requires=['requests', 'statsmodels', 'matplotlib','numpy','scikit-learn','scipy','colourmap>=1.1.10','pandas','tqdm','scatterd>=1.2.5', 'adjustText'],
      python_requires='>=3',
      name='pca',
      version=new_version,
